5ced5de67802f5332ba83e0cededc0ef3c131bd9
fare cruz <cruzjocelito97@gmail.com>
2024-05-30 13:15:51 -0400

    Hemos arreglado los errores y puesto la animacion, ya todo esta listo, ahora le arregaremos el modo movil

--- Newindex.js
+++ Newindex.js
@@ -1,13 +1,15 @@
 var Input = document.getElementById('Input'); 
 var Button = document.getElementById('Button'); 
+
 // Selecion del input y button
 
 // Selecion de cajas; 
 var WillDo = document.getElementById('will-do');
 var Doing = document.getElementById('doing'); 
 var finish = document.getElementById('finish'); 
+var eliminados = document.getElementById('Eliminados')
 
-Button.addEventListener('click', () => {
+function escuchar(){
 
 let Data = Input.value; 
 if(Data.trim() === ""){
@@ -16,61 +18,79 @@ return console.log('escriba algo')
 // Si el input esta vacio no devolvera nada; 
 
 let cosa = document.createElement('div'); 
+cosa.className= 'Cosa'; 
+
+let Box_Eliminar= document.createElement('div'); 
+let Box_p = document.createElement('div'); 
+Box_p.className = 'Box_p'; 
+Box_Eliminar.className= 'Box_Eliminar'
+
 let p_cosa = document.createElement('p');
 p_cosa.textContent = document.getElementById('Input').value; 
+Box_p.appendChild(p_cosa); 
 
-let b_eliminar = document.createElement('button'); 
-b_eliminar.textContent = 'Eliminar'; 
-let b_two = document.createElement('button'); 
-b_two.textContent = 'Empezar'; 
+let b_eliminar = document.createElement('span'); 
+b_eliminar.textContent = 'X'; 
+Box_Eliminar.appendChild(b_eliminar); 
 
 // Le decimos que al presionar eliminar elimine toda la lista
-b_eliminar.addEventListener('click',() => {cosa.remove()})
+b_eliminar.addEventListener('click',() => {
+cosa.remove()
 
-// cuando le de click a empezar se quitara el boto de empesar y se pondra otro boto llamado 'terminar'
-b_two.addEventListener('click',() => {
-b_two.remove(); 
-let terminar= document.createElement('button'); 
-terminar.textContent = 'terminar'
-cosa.appendChild(terminar)
-Doing.appendChild(cosa);
+let end = document.createElement('div'); 
+let cj_one = document.createElement('div'); 
+let cj_two = document.createElement('div'); 
+cj_one.className = 'cj_one'; 
+cj_two.className = 'cj_two'; 
+end.className = 'end'; 
 
-// cuando se le de a terminar este se eliminara y pongra otro boto llamado reanidar y pasara a Finish
-terminar.addEventListener('click',() => {finish.appendChild(cosa);
-terminar.remove(); 
-let reanudar = document.createElement('button'); 
-reanudar.textContent = 'Reanudar'; 
-cosa.appendChild(reanudar)
+let P_end = document.createElement('span'); 
+let revertir = document.createElement('span'); 
+revertir.textContent = 'X'; 
+P_end.textContent = `Se a eliminado ${Data}`; 
+cj_one.appendChild(P_end); 
+cj_two.appendChild(revertir);
 
-// cuando se le de a reaundar se eliminara y volvera terminar y se pondra en la casa Doing 
-reanudar.addEventListener('click', () => {
-reanudar.remove(); 
-cosa.appendChild(terminar);
-Doing.appendChild(cosa)})
+revertir.addEventListener('click', ()=> {
+    WillDo.appendChild(cosa); 
+    end.remove()
+})
 
-})  // <= este es de terminar 
+end.appendChild(cj_one); 
+end.appendChild(cj_two);
+eliminados.appendChild(end); 
 
-}) // <= este es de b_two
+
+
+setTimeout(() => {
+eliminados.removeChild(end); 
+
+}, 3000)
+
+})
 
 
 p_cosa.addEventListener('dblclick', () => {
 
 
-let b_three = document.createElement('button'); 
-b_three.textContent ='Asignar'; 
+let b_three = document.createElement('Span'); 
+b_three.textContent ='✔'; 
+b_three.className = 'asignar'; 
 
 let New_cosa = p_cosa.textContent;
 
 let in_two = document.createElement('input'); 
 in_two.type = 'text'; 
+in_two.className = 'Editar';
 in_two.value = New_cosa; 
 
-cosa.replaceChild(in_two, p_cosa);
-// cosa.appendChild(b_three);
-in_two.insertAdjacentElement('afterend', b_three);
+// cosa.replaceChild(in_two, p_cosa);
 
+
+Box_p.appendChild(in_two); 
+Box_Eliminar.appendChild(b_three);
 b_eliminar.remove(); 
-// b_two.remove(); 
+p_cosa.remove(); 
 
 b_three.addEventListener('click', () => {
 
@@ -78,24 +98,44 @@ b_three.addEventListener('click', () => {
 let datap = in_two.value; 
 p_cosa.textContent = datap;
 b_three.remove(); 
+in_two.remove()
 
-cosa.replaceChild(p_cosa, in_two);
-cosa.appendChild(b_eliminar);
+// cosa.replaceChild(p_cosa, in_two);
+Box_p.appendChild(p_cosa)
+Box_Eliminar.appendChild(b_eliminar);
 
 })
 
 console.log('se puede editar')
 })
 
-
-cosa.appendChild(p_cosa);
-cosa.appendChild(b_eliminar);
-cosa.appendChild(b_two);
+cosa.appendChild(Box_p);
+cosa.appendChild(Box_Eliminar);
 WillDo.appendChild(cosa)
 
 // Creamos el tex y lo asignamos a Li y creamos los boteliminars y todos iran dentro de WilDo. 
 
 
 Input.value = ''
+}
+
+Button.addEventListener('click', escuchar); 
+Input.addEventListener('keypress', (event) => {
+if(event.key === 'Enter') {
+    escuchar(); 
+}
 })
 
+Sortable.create(Doing,{
+group: 'Shared', 
+Animation: 150,
+})
+Sortable.create(WillDo, {
+group: 'Shared', 
+Animation: 150
+})
+
+Sortable.create(finish,{
+group: 'Shared', 
+Animation: 150
+})
